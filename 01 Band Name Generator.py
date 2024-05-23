3c84fd8390b58dffdb3d83b0b072c587b2ad3f1f
Daniel Escoval <34299110+Doun92@users.noreply.github.com>
2024-05-23 22:11:26 +0200

    Add files via upload

---
+++ 01 Band Name Generator.py
@@ -0,0 +1,11 @@
+#TODO: Ask a city
+
+#TODO Ask a pet nam
+
+#TODO: Display the result
+
+#TODO: Ask to save the name in a txt file
+
+#TODO: Create or incremente the file
+
+#TODO: Ask if start again or not
