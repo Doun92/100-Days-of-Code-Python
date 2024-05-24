348e7feb270f6b53779a2b83d3b40137e04b29a2
Daniel Escoval <34299110+Doun92@users.noreply.github.com>
2024-05-24 13:58:29 +0200

    add some inputs

--- 01 Band Name Generator.py
+++ 01 Band Name Generator.py
@@ -1,11 +1,30 @@
-#TODO: Ask a city
+
 
 #TODO Ask a pet nam
+pet_name = input("Please, chose a pet name: ")
+
+#TODO: Ask a city
+city = input("Please, chose a city: ")
 
 #TODO: Display the result
-
-#TODO: Ask to save the name in a txt file
+print(f"Your band name is \"{city} {pet_name}\"")
 
 #TODO: Create or incremente the file
+def write_file():
+    pass
 
 #TODO: Ask if start again or not
+def try_again():
+    pass
+
+#TODO: Ask to save the name in a txt file
+def question_save_file():
+    save_file = input("Do you want to save the name ?")
+    # if the answer is affirmative
+    if save_file in ["T", "t", "y", "yes", "true", "Yes", "o", "oui"]:
+        write_file()
+    if save_file in ["F", "f", "n", "no", "false", "No", "non"]:
+        try_again()
+
+
+#TODO: Begin by asking if the want to take a random input from the file
