9218d0c0f4da185c7a88b52eee10691fc2781e98
Daniel Escoval <34299110+Doun92@users.noreply.github.com>
2024-05-27 15:33:09 +0200

    Add files via upload

---
+++ 003 Treasure Island.py
@@ -0,0 +1,15 @@
+user_position = (0,0)
+treasure_position = (-5,5)
+size = (10,10,-10,-10)
+
+def action():
+    pass
+
+def hint():
+    pass
+
+def move(direction,steps):
+    pass
+
+def check_position(user, treasure):
+    pass
