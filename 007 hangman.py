8f843445d075cfa5a8e4d95eab9d6f85872846f0
Daniel Escoval <34299110+Doun92@users.noreply.github.com>
2024-06-19 22:08:11 +0200

    Add files via upload

--- 007 hangman.py
+++ 007 hangman.py
@@ -1,7 +1,6 @@
 word = "bosquet"
-
 used_letters = []
-
+correct_letters = []
 
 def get_user_choice():
     while True:
@@ -14,5 +13,22 @@ def get_user_choice():
             
         return user_choice
 
+def check_right(user, word, correct_letters):
+    if user in list(word):
+        correct_letters.append(user)
+
+
+
+def check_letter_used(user,used_letters):
+    if user in used_letters:
+        print("Letter already used.")
+        print(f"Here are the letters used: \n ***\n{used_letters}\n***")
+    else:
+        used_letters.append(user)
+    # print(used_letters)
+    check_right(user, word, correct_letters)
+
 user_lettter = get_user_choice()
-print(user_lettter)
+# print(user_lettter)
+
+check_letter_used(user_lettter, used_letters)
