91d11dc585fb8b02632262d0570179ed17077442
Daniel Escoval <34299110+Doun92@users.noreply.github.com>
2024-06-23 13:48:25 +0200

    Fin

--- 007 hangman.py
+++ 007 hangman.py
@@ -6,24 +6,30 @@ mot = choice(liste_mots)
 fin_jeu = False
 
 print(mot)
-
 display = []
 longueur_mot = len(mot)
 for lettre in mot:
     display += "_"
 print(display)
 
+vies = 6
+
 while fin_jeu == False:
     choix = input("Choisis une lettre: ")
 
+    #Vérification de la lettre
     for position in range(longueur_mot):
         lettre = mot[position]
         if choix == lettre:
             display[position] = lettre
-        else:
-            # print("Non")
-            pass
+
+    if choix not in mot:
+        vies -= 1
+        if vies == 0:
+            fin_jeu = True
+            print("Tu as perdu")
 
     print(display)
     if "_" not in display:
         fin_jeu = True
+        print("Tu as deviné le mot")
