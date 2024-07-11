838d1bd03ec3d55e18e5be30de2fdb104dc362bc
Daniel Escoval <34299110+Doun92@users.noreply.github.com>
2024-07-11 20:13:59 +0200

    finished

--- 008 caesar_cypher.py
+++ 008 caesar_cypher.py
@@ -9,20 +9,26 @@ for i, lettre in enumerate(liste_lettres):
 
 print(empty_dict)
 
+def get_key(val, dict):
+    for key, value in dict.items():
+        if val == value:
+            return key
+    return "key doesn't exist"
+
 def encodeMessage(m,c):
     
     encoded_message = ""
     for lettre in m:
         # print(empty_dict[lettre])
-        print(empty_dict[lettre]+c)
+        # print(empty_dict[lettre]+c)
         if (empty_dict[lettre]+c) > len(empty_dict):
-            print("oui")
-        
+            new_result = empty_dict[lettre]+c - len(empty_dict)
+            encoded_message += get_key(new_result,empty_dict)
+        else:
+          encoded_message += get_key(empty_dict[lettre]+c,empty_dict)
+
     return encoded_message
 
 encoded_message = encodeMessage(message,change)
 
-print(encoded_message)   
-#zhsba
-
-#https://www.geeksforgeeks.org/python-get-key-from-value-in-dictionary/
+print(encoded_message)
